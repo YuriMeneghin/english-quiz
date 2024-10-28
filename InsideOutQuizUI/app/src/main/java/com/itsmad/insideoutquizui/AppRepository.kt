32fc0d071cfe75ff71309dab5a71fc68daf55c40
YuriMeneghin <yuri.meneghin@edu.itspiemonte.it>
2024-10-28 10:28:48 +0100

    update

---
+++ InsideOutQuizUI/app/src/main/java/com/itsmad/insideoutquizui/AppRepository.kt
@@ -0,0 +1,10 @@
+package com.itsmad.insideoutquizui
+
+object AppRepository {
+    // risposte prima domanda
+    var q1_answer1: Boolean = false
+    var q1_answer2: Boolean = false
+    var q1_answer3: Boolean = false
+    var q1_answer4: Boolean = false
+
+}
